28a146220bcb9eb7f6f37e0a97d84cae4d369d7a
unknown <migtarx@gmail.com>
2022-02-01 11:10:24 +0100

    feat: working api and client
    Fully functional rest api server and client side | TODO: Handle all servers URL's (maybe simple sql table with DAO). Optimize check status request

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', function (req, res) {
+    res.sendStatus(200);
+})
+
+app.listen(PORT, () => {
+    console.log(`Now listening to requests on port ${PORT}`);
+});
