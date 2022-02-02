aff14580bf2f98c32c66f9deeec9c755e19240a3
Miguel Puerta <migtarx@gmail.com>
2022-02-02 21:56:31 +0100

    fix: Front and back fixes
    Back: Fixed error logging in @fsockopen() method | Front: Refactored UI data retriving info

--- client/data/index.php
+++ client/data/index.php
@@ -4,7 +4,7 @@ $urls = array("mpuerta.com", "mpuerta.com", "mpuerta.com", "mpuerta.com");
 $ports = array(8222, 8223, 8224, 8225);
 
 for ($i = 0; $i <= count($urls) - 1; $i++) {
-    if (fsockopen($urls[$i], $ports[$i], $errno, $errstr, 1)){
+    if (@fsockopen($urls[$i], $ports[$i], $errno, $errstr, 1)){
         $status = $status . "online:";
     } else {
         $status = $status . "offline:";
--- client/js/retrieveStatus.js
+++ client/js/retrieveStatus.js
@@ -1,43 +1,69 @@
 fetchData()
-setInterval('fetchData()', 10000);
+setInterval('fetchData()', 30000);
+
 
 function fetchData(){
     $.ajax(
-        'https://status.xnorm.cloud/data/', {
+        //https://status.xnorm.cloud/data/
+        'http://localhost/xnorm-cloud-status/client/data/', {
             success: function(data) {
                 getCurrentDate()
                 var statusStr = data.slice(0, -1);
                 var status = statusStr.split(":");
                 for (let i = 0; i < status.length; i++) {
                     if (status[i] == "online") {
-                        $(`#sv${i + 1}div`).removeClass("chck");
-                        $(`#sv${i + 1}div`).removeClass("non-chck");
-                        $(`#sv${i + 1}div`).removeClass("non");
+                        $(`#sv${i + 1}div`).removeClass("chck non-chck non");
                         $(`#sv${i + 1}div`).addClass('chck');
                         $(`#sv${i + 1}txt`).text('Online');
+                        $(`#status-info`).html("IDENTIFIED <i class=\"fas fa-check\"></i>");
+                        $(`#status-info`).css("background", "#00BCD4");
+                        $(`.tt-time-status:after`).css("background", "#00BCD4");
+                        conecting();
                     } else {
-                        $(`#sv${i + 1}div`).removeClass("chck");
-                        $(`#sv${i + 1}div`).removeClass("non-chck");
-                        $(`#sv${i + 1}div`).removeClass("non");
+                        $(`#sv${i + 1}div`).removeClass("chck non-chck non");
                         $(`#sv${i + 1}div`).addClass('non-chck');
                         $(`#sv${i + 1}txt`).text('Offline');
+                        $(`#status-info`).html("IDENTIFIED <i class=\"fas fa-check\"></i>");
+                        $(`#status-info`).css("background", "#00BCD4");
+                        $(`.tt-time-status:after`).css("background", "#00BCD4");
+                        conecting();
                     }
                 }
             },
             error: function() {
-                $(`#sv${i + 1}div`).removeClass("chck");
-                $(`#sv${i + 1}div`).removeClass("non-chck");
-                $(`#sv${i + 1}div`).removeClass("non");
-                $(`#sv${i + 1}div`).addClass('non-chck');
-                $(`#sv${i + 1}txt`).text('Can\' get status');
+                $(`#sv1div,#sv2div,#sv3div,#sv4div`).each(function(){
+                    $(this).removeClass("chck non-chck non");
+                    $(this).addClass('non-chck');
+                });
+                $(`#sv1txt,#sv2txt,#sv3txt,#sv4txt`).each(function(){
+                    $(this).text('Can\' get status');
+                });
+                conecting();
             }
         }
     );
 }
 
+function conecting(){
+    setTimeout(function () {
+        $(`#status-info`).text("Identifying...");
+        $(`#status-info`).css("background", "#f59b42");
+        $(`.tt-time-status:after`).css("background", "#f59b42");
+    }, 24000);
+    
+}
+
 function getCurrentDate(){
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    $(`#current-time`).text(`${date} ${time}`);
+    Date.prototype.current = function() {
+        var yyyy = this.getFullYear().toString();
+        var mm = (this.getMonth()+1).toString();
+        var dd  = this.getDate().toString();
+        var currentHours = this.getHours();
+        var varcurrentMinutes = this.getMinutes();
+        var currentSeconds = this.getSeconds();
+        return (dd[1]?dd:"0"+dd[0]) + "/" + (mm[1]?mm:"0"+mm[0]) + "/" +  yyyy + " " + ("0" + currentHours).slice(-2) + ":" + ("0" + varcurrentMinutes).slice(-2) + ":" + ("0" + currentSeconds).slice(-2);
+    };
+       
+    var date = new Date();
+    $(`#current-time`).text(`Last check: ${date.current()}`);
 }
